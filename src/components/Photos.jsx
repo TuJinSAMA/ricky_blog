1e5f77a75bd596e0c7c58b1cf2cc019b6afdba04
gongyuqi <gong_yu_qi@163.com>
2023-12-07 17:43:47 +0800

    initialize project

---
+++ src/components/Photos.jsx
@@ -0,0 +1,91 @@
+'use client'
+
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import React from 'react'
+
+import image1 from '@/images/photos/image-1.jpg'
+import image2 from '@/images/photos/image-2.jpg'
+import image3 from '@/images/photos/image-3.jpg'
+import image4 from '@/images/photos/image-4.jpg'
+import image5 from '@/images/photos/image-5.jpg'
+
+const images = [image1, image2, image3, image4, image5]
+const alts = [
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+]
+
+export function Photos() {
+  const [width, setWidth] = React.useState(0)
+  const [isCompact, setIsCompact] = React.useState(false)
+  const expandedWidth = React.useMemo(() => width * 1.38, [width])
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      // 640px is the breakpoint for md
+      if (window.innerWidth < 640) {
+        setIsCompact(true)
+        return setWidth(window.innerWidth / 2 - 64)
+      }
+
+      setWidth(window.innerWidth / images.length - 4 * images.length)
+    }
+
+    window.addEventListener('resize', handleResize)
+    handleResize()
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return (
+    <motion.div
+      className="mt-16 sm:mt-20"
+      initial={{ opacity: 0, scale: 0.925, y: 16 }}
+      animate={{ opacity: 1, scale: 1, y: 0 }}
+      transition={{
+        delay: 0.5,
+        type: 'spring',
+      }}
+    >
+      <div className="flex justify-start w-full gap-4 px-4 py-4 -my-4 overflow-x-auto snap-x snap-proximity scroll-pl-4 sm:gap-6 md:justify-center md:overflow-x-hidden md:px-0">
+        {images.map((image, idx) => (
+          <motion.div
+            key={image.src}
+            className="relative flex-none h-40 overflow-hidden shrink-0 snap-start rounded-xl bg-zinc-100 ring-2 ring-lime-800/20 dark:bg-zinc-800 dark:ring-lime-300/10 md:h-72 md:rounded-3xl"
+            animate={{
+              width,
+              opacity: isCompact ? 1 : 0.85,
+              filter: isCompact ? 'grayscale(0)' : 'grayscale(0.5)',
+              rotate: idx % 2 === 0 ? 2 : -1,
+            }}
+            whileHover={
+              isCompact
+                ? {}
+                : {
+                    width: expandedWidth,
+                    opacity: 1,
+                    filter: 'grayscale(0)',
+                  }
+            }
+            layout
+          >
+            <Image
+              src={image}
+              alt={alts[idx] ?? ''}
+              sizes="(min-width: 640px) 18rem, 11rem"
+              className="absolute inset-0 object-cover w-full h-full pointer-events-none select-none"
+              priority
+            />
+          </motion.div>
+        ))}
+      </div>
+    </motion.div>
+  )
+}
